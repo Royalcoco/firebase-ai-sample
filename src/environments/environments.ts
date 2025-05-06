1280a41894b3a2a6c38f3d8ab7ca805d57f74eb8
saliba.roy.pro <saliba.roy.pro@gmail.com>
2025-05-06 19:56:04 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyAdLy8Bgw3xzy_2ny9fJCg--GLZhBfd-H4',
+      authDomain: 'sample-firebase-ai-app-c6c59.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-c6c59',
+      storageBucket: 'sample-firebase-ai-app-c6c59.firebasestorage.app',
+      messagingSenderId: '325618577120',
+      appId: '1:325618577120:web:9c2df4ab003117ddf76a85',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyC5OSZqJitdR0AN4nQvpPogBHOMNYHPtzA',
+  debug_token: ''
+};
